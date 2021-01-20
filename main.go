8e4e2835fd826794f8b91a00791b8365a2e6eda5
Martin <tatodarosa97@gmail.com>
2021-01-20 11:26:50 -0300

    Basic endopint complete

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -45,7 +46,7 @@ func initDB() {
 		panic("Fallo la conexion con la base de datos")
 	}
 
-	db.Exec("CREATE DATABASE orders_db")
+	db.Exec("CREATE DATABASE IF NOT EXISTS orders_db")
 
 	db.Exec("USE orders_db")
 	db.AutoMigrate(&Pedido{}, &Producto{})
@@ -57,10 +58,61 @@ func getPedidos(w http.ResponseWriter, r *http.Request) {
 	db.Preload("Productos").Find(&pedidos)
 	json.NewEncoder(w).Encode(pedidos)
 }
+
+func getPedido(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "aplication/json")
+	//Se cargan las variables
+	params := mux.Vars(r)
+	//Se obtiene la id
+	pedidoID := params["id"]
+
+	var pedido Pedido
+	db.Preload("Items").First(&pedido, pedidoID)
+	json.NewEncoder(w).Encode(pedido)
+
+}
+
+func createPedido(w http.ResponseWriter, r *http.Request) {
+	var pedido Pedido
+
+	json.NewDecoder(r.Body).Decode(&pedido)
+	db.Create(&pedido)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pedido)
+}
+
+func updatePedido(w http.ResponseWriter, r *http.Request) {
+	var updatedPedido Pedido
+
+	json.NewDecoder(r.Body).Decode(&updatedPedido)
+
+	db.Save(&updatedPedido)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedPedido)
+}
+func deletePedido(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	pedidoID := params["id"]
+	id64, _ := strconv.ParseUint(pedidoID, 10, 24)
+
+	pedidoIDu := uint(id64)
+
+	db.Where("pedido_id = ?", pedidoIDu).Delete(&Producto{})
+	db.Where("pedido_id = ?", pedidoIDu).Delete(&Pedido{})
+	w.WriteHeader(http.StatusNoContent)
+
+}
 func main() {
 	router := mux.NewRouter()
 	//Get Pedios
 	router.HandleFunc("/api/pedidos", getPedidos).Methods("GET")
+	router.HandleFunc("/api/pedido/{id}", getPedido).Methods("GET")
+	router.HandleFunc("/api/pedido", createPedido).Methods("POST")
+	router.HandleFunc("/api/pedido/{id}", updatePedido).Methods("PUT")
+	router.HandleFunc("/api/pedido/{id}", deletePedido).Methods("DELETE")
+
 	initDB()
 	port := ":8080"
 	fmt.Println("Api Runing Under ")
